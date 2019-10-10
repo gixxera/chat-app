84de704e78455cf2356d77bc3c048d66d3f2f881
Milen <monioo@abv.bg>
2019-10-10 11:19:47 +0300

    Setup port and dev scripts

--- src/server.js
+++ src/server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+const port = process.env.PORT || 3000;
+
 const publicPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
