c0d079036298c7238b8fcf789a17f9213f925b2a
Milen <monioo@abv.bg>
2019-10-10 14:46:08 +0300

    Add message form and event

--- public/src/chat.js
+++ public/src/chat.js
@@ -1,1 +1,19 @@
-io();
+const socket = io();
+
+socket.on('message', (message) => {
+  console.log(message);
+});
+
+document.querySelector('#msg-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const inputValue = e.target.elements.message.value;
+  socket.emit('sendMessage', inputValue);
+});
+
+// socket.on('countUpdated', (count) => {
+//   console.log('The count has been updated!', count);
+// });
+
+// document.querySelector('#increment').addEventListener('click', () => {
+//   socket.emit('increment');
+// });
